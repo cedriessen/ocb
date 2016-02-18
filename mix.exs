3edd468ded3240f4147ae53d516b13bad1047cef
Christoph E. Driessen <cdriessen@extron.com>
2016-02-18 15:57:55 +0100

    group configuration of project dependencies into dedicated functions
    No more split up between entries in the project and deps functions.

--- mix.exs
+++ mix.exs
@@ -3,13 +3,17 @@ defmodule Ocb.Mixfile do
 
   def project do
     [
-      app: :ocb,
-      version: "0.0.1",
-      elixir: "~> 1.2",
-      deps: deps
+      [
+        app: :ocb,
+        version: "0.0.1",
+        elixir: "~> 1.2",
+        deps: deps
+      ],
+      exdoc,
+      escript,
+      inch
     ]
-    ++ exdoc.project
-    ++ escript.project
+    |> merge
   end
 
   # Configuration for the OTP application
@@ -19,33 +23,47 @@ defmodule Ocb.Mixfile do
     [applications: [:logger, :porcelain, :timex]]
   end
 
+  #
+  #
+  #
+
   # ExDoc config
   defp exdoc do
-    %{project: [
-        # ExDoc
-        name: "ocb",
-        source_url: "https://github.com/cedriessen/ocb",
-        docs: [
-  #        logo: "path/to/logo.png",
-          extras: ["README.md", "LICENSE.md"]
-        ]
+    [
+      # ExDoc
+      name: "ocb",
+      source_url: "https://github.com/cedriessen/ocb",
+      docs: [
+#        logo: "path/to/logo.png",
+        extras: ["README.md", "LICENSE.md"]
       ],
       deps: [
         {:earmark, "~> 0.1", only: :dev},
         {:ex_doc, "~> 0.11", only: :dev}
       ]
-    }
+    ]
+  end
+
+  # InchEx config
+  # update with
+  # $ MIX_ENV=docs mix inch.report
+  # or simply
+  # $ mix inch.report
+  defp inch do
+    [
+      preferred_cli_env: ["inch.report": :docs],
+      deps: [{:inch_ex, ">= 0.0.0", only: :docs}]
+    ]
   end
 
   # escript config
   # create a standalone application with escript
   defp escript do
-    %{project: [
-        escript: [main_module: Ocb],
-        build_embedded: Mix.env == :prod,
-        start_permanent: Mix.env == :prod,
-      ]
-    }
+    [
+      escript: [main_module: Ocb],
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -66,10 +84,22 @@ defmodule Ocb.Mixfile do
       # Use Tzdata 0.1.8 since the latest version has issues with escript builds.
       # See https://github.com/bitwalker/timex/issues/86
       {:tzdata, "~> 0.1.8", override: true},
-      # update with
-      # $ MIX_ENV=docs mix inch.report
-      {:inch_ex, ">= 0.0.0", only: :docs}
     ]
-    ++ exdoc.deps
   end
+
+  #
+  #
+  #
+
+  # Merge a list of keyword lists into one.
+  # The values of equal keys are merged into a list.
+  defp merge(deps) do
+    deps
+    |> Enum.reduce(fn dep, acc -> Keyword.merge(dep, acc, &merge/3) end)
+  end
+
+  defp merge(_k, v1, v2) when is_list(v1) and is_list(v2), do: v1 ++ v2
+  defp merge(_k, v1, v2) when is_list(v1), do: [v2 | v1]
+  defp merge(_k, v1, v2) when is_list(v2), do: [v1 | v2]
+  defp merge(_k, v1, v2), do: [v1, v2]
 end
